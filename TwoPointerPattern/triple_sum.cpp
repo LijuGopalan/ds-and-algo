702e8bfdd261fb07409b28f71ba599c6270b372d
Liju Gopalan <liju84@gmail.com>
2025-02-09 13:21:38 -0600

    triple cum

--- TwoPointerPattern/triple_sum.cpp
+++ TwoPointerPattern/triple_sum.cpp
@@ -13,6 +13,8 @@ Example 2:
 Input: [-5, 2, -1, -2, 3]
 Output: [[-5, 2, 3], [-2, -1, 3]]
 Explanation: There are two unique triplets whose sum is equal to zero.
+ 
+ -3 -2 -1 0 1 2 
 
 */
 
@@ -20,20 +22,54 @@ Explanation: There are two unique triplets whose sum is equal to zero.
 #include <set>
 #include <vector>
 #include <array>
+#include <map>
+#include <map>
+
 using namespace std;
 
-
-void sort_array_to_set(vector<int>& a, set<int>& s) {
+void sort_array_and_store_in_map(vector<int>& a, multimap<int,bool>& m) {
     for (int i = 0; i < a.size(); i++) {
-        s.insert(a[i]);
+        m.insert({a[i],false});
+        //m[a[i]] = false;
+    } 
+}
+
+void find_all_unique_triplets(vector<int>& arr, vector<array<int, 3>>& triplets) {
+
+    multimap<int,bool> m;
+
+    sort_array_and_store_in_map(arr, m); 
+ 
+    multimap<int,bool>::iterator itra = m.begin();
+
+    multimap<int,bool>::iterator itr;
+    while (itra != m.end())
+    {
+        multimap<int,bool>::iterator itrb = m.end();
+        while (itrb != m.begin())
+        {
+            int sum = itrb->first + itra->first;
+            
+            itr = m.find(-sum);            
+
+            if(itr != m.end() && itr != itrb && itr != itra) {
+
+                if( itra->second == false || itrb->second == false || itr->second == false ) {
+                    array<int, 3> triplet = {itra->first,itr->first,itrb->first};
+                    triplets.push_back(triplet);
+                    itra->second = true;
+                    itrb->second = true;
+                    itr->second = true;
+                }
+                
+            } 
+            --itrb;
+        }
+
+        itra++;
+
     }
 
-    cout << "Set: " << endl;
-    for (set<int>::iterator it = s.begin(); it != s.end(); it++) {
-        cout << *it << " ";
-    }
-    cout << endl;
-    //-3 -2 -1 0 1 2
 }
 
 
@@ -42,42 +78,26 @@ int main() {
     std::cout << " Triplet Sum to Zero " << std::endl;
 
     vector<int> arr = {-3, 0, 1, 2, -1, 1, -2};
-    
-    set<int> s;
-    sort_array_to_set(arr, s);
-
-    //set has the elements in sorted order now. 
-    //use the set to find the triplets.
-    //set sorted the data and removed the duplicates. 
-
     vector<array<int, 3>> triplets;
-
-    set<int>::iterator itb = s.begin();
-    int size = s.size();
+    find_all_unique_triplets(arr,triplets);
     
-    while (size > 0)
-    {
-        set<int>::iterator ite = s.end();
-
-        while (itb != ite)
-        {
-            int sum = *itb + *ite;
-            int target = -sum;
-            if(s.find(target) != s.end() && s.find(target) > itb && s.find(target) < ite) {
-                array<int, 3> triplet = {*itb, target, *ite};
-                triplets.push_back(triplet);
-            }
-            ite--;
-        }
-
-        itb++;size--;
-    
-    }
-
     cout << "Triplets: " << endl;
     for (int i = 0; i < triplets.size(); i++) {
         cout << triplets[i][0] << " " << triplets[i][1] << " " << triplets[i][2] << endl;
     }
+
+
+    arr.clear();
+    arr = {-5, 2, -1, -2, 3};
+    triplets.clear();
+    find_all_unique_triplets(arr,triplets);
     
-    return 0;
+    cout << "Triplets: " << endl;
+    for ( int i = 0; i < triplets.size(); i++) {
+        cout << triplets[i][0] << " " << triplets[i][1] << " " << triplets[i][2] << endl;
+    }
+    
+    return 1;
 }
+
+
